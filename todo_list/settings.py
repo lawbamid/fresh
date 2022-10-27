324ab2fc6a03c7b7145ca22d9682ba409efc4f45
lawbamid <109281990+lawbamid@users.noreply.github.com>
2022-10-27 11:25:19 +0100

    new task

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kl-x*-9q&vxmx&&6%1tts#&b5yofqvxu**kpj109jv$f*sd#(@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["fresh-todo.herokuapp.com","127.0.0.1"]
+ALLOWED_HOSTS = ["fresh-todo01.herokuapp.com","127.0.0.1"]
 
 
 # Application definition
